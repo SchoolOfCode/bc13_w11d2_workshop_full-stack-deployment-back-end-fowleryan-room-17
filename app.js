49e00821db544bdb02a55754c02a46e82b37dc8c
Fowl3r <tcd.fowler@gmail.com>
2022-12-06 15:21:36 +0000

    cors allowed access for all

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import shoppingListRouter from "./routes/shoppingList.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
